cd7c4780b26895142316b7fd375d9999bc3e400b
kewin_Abreu <87028430+KewinAbreuu@users.noreply.github.com>
2022-06-13 20:27:41 -0300

    [KewinAbreu] - Page Home e Sobre

---
+++ site/src/Componets/BottomBar/index.js
@@ -0,0 +1,16 @@
+
+import React,{useState} from "react";
+import { Container } from './style';
+
+
+export default function BottomBar() {
+  return (
+      <>
+        <Container>
+            
+        </Container>
+      </>
+  );
+}
+
+
---
+++ site/src/Componets/BottomBar/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items:center;
+  flex-direction:column;
+
+  width: 400px;
+  height: 68px;
+
+  background-color: rgba(0, 0, 0, 0.4);;
+  backdrop-filter: blur(10px);
+
+  position: fixed;
+  bottom: 0;
+  margin-bottom: 20px;
+
+  border-radius: 50px;
+`
